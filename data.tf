ea31f0c58bdc10bead0e919cdde98e76669b7580
Lee Hull <lee5i3@leehull.com>
2020-03-01 14:29:52 -0500

    Changes unrelated to a task

--- data.tf
+++ data.tf
@@ -3,3 +3,9 @@
 #--------------------------------------------------------------
 data "aws_caller_identity" "current" {
 }
+
+data "aws_route53_zone" "domain" {
+  count = var.enabled ? 1 : 0
+
+  name = "${var.route53_domain}."
+}
